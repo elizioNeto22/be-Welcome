896848dc873bb027ee164098298336b457256a1f
Elizio Neto <epe_elizio@hotmail.com>
2020-01-29 22:51:40 -0300

    little update

--- src/components/custom-button/customButton-component.jsx
+++ src/components/custom-button/customButton-component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import './custom-button-styles.scss'
-const CustomButton = ({children, isGoogleSignIn, handleClick, ...otherProps}) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : 'custom-button'}`} onClick={() => handleClick} {...otherProps}>{children}</button>
+const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
+  <button className={`${isGoogleSignIn ? 'google-sign-in' : 'custom-button'}`} {...otherProps}>{children}</button>
 )
 
 export default CustomButton
