8ad794706e004cdccb243262e82330b0aadcf273
Elizio Neto <epe_elizio@hotmail.com>
2020-01-29 22:54:35 -0300

    added firestore to manage users data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,31 +5,63 @@ import 'firebase/firestore'
 
 
 const config = {
-  apiKey: "AIzaSyDmGIs-lFeAbjd_zbYjWydFhKW-neIa7js",
-  authDomain: "be-welcome.firebaseapp.com",
-  databaseURL: "https://be-welcome.firebaseio.com",
-  projectId: "be-welcome",
-  storageBucket: "be-welcome.appspot.com",
-  messagingSenderId: "601633494345",
-  appId: "1:601633494345:web:e575764d4da6d7e253b698",
-  measurementId: "G-SWD0KTDMDW"
+apiKey: "AIzaSyDmGIs-lFeAbjd_zbYjWydFhKW-neIa7js",
+authDomain: "be-welcome.firebaseapp.com",
+databaseURL: "https://be-welcome.firebaseio.com",
+projectId: "be-welcome",
+storageBucket: "be-welcome.appspot.com",
+messagingSenderId: "601633494345",
+appId: "1:601633494345:web:e575764d4da6d7e253b698",
+measurementId: "G-SWD0KTDMDW"
 }
 
 firebase.initializeApp(config)
 const auth = firebase.auth()
 const firestore = firebase.firestore()
 
-
+// auth
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({'propmt': 'select_account)'}) // 1
 const signInWithGoogle = () => auth.signInWithPopup(provider) // 2
 const userSignOut = () => auth.signOut()
 
+
+//firestore
+const addUserToDatabase = async (userAuth, additionalData) => {
+  if(!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const userSnapShot = await userRef.get()
+
+  if(!userSnapShot.exists) {
+    const {displayName, email} = userAuth
+    const createdAt = new Date()
+    
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+      
+    } catch(error) {
+      console.log('Error creating user. ', error.message)
+    }
+  }
+
+return userRef
+}
+
+
+
 export {
-  auth,
-  firestore,
-  signInWithGoogle,
-  userSignOut
+    auth,
+    firestore,
+    signInWithGoogle,
+    userSignOut,
+    addUserToDatabase
 }
 
 export default firebase
