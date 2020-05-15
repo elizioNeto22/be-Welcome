e39deced2543a6c60190502b0848b212b5c0c05e
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-15 10:54:34 -0300

    functions for add and delete cart item for better read on reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,1 +1,19 @@
-// const addItem = (item) =>
+export const addItem = (itemToAdd, cartItems) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === itemToAdd.id
+  )
+
+  if (existingItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    )
+  }
+  return [...cartItems, { ...itemToAdd, quantity: 1 }]
+}
+
+export const deleteItem = (itemToDelete, cartItems) =>
+  cartItems.filter((cartItem) => cartItem.id !== itemToDelete.id)
+
+export const removeUnity = ''
