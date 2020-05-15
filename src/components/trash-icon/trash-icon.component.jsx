b4abd71cb7425bfa15016eba2b151b3169b92442
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-15 10:53:07 -0300

    icons updates

--- src/components/trash-icon/trash-icon.component.jsx
+++ src/components/trash-icon/trash-icon.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { deleteItem } from '../../redux/cart/cart.actions'
 
 import { ReactComponent as Trash } from '../../assets/icons/trash.svg'
 
-const TrashIcon = () => (
-  <div className="trash">
-    <Trash />
+const TrashIcon = ({ deleteItem, item }) => (
+  <div onClick={() => deleteItem(item)}>
+    <Trash className="trash" />
   </div>
 )
-export default TrashIcon
+
+const mapStateToProps = {
+  deleteItem,
+}
+
+export default connect(null, mapStateToProps)(TrashIcon)
