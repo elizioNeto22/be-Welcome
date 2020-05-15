cd14c056ec5a2cdaf8c7e2bb22fa293c095081e0
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-15 10:55:05 -0300

    cart expand and total selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,7 +12,17 @@ export const selectHiddenCart = createSelector(
   (cart) => cart.hidden
 )
 
+export const selectExpand = createSelector([selectCart], (cart) => cart.expand)
+
+export const selectExpandCart = createSelector([selectExpand], (expand) =>
+  expand ? 'expand' : ''
+)
+
 export const selectCartItemCount = createSelector(
   [selectCartItems],
   (cartItems) => cartItems.reduce((acc, item) => acc + item.quantity, 0)
+)
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
 )
